3008a99a43d34339aac2c9e823f7ae04f0d850d8
francastelobranco <francy.cb2@gmail.com>
2023-06-06 15:57:14 -0300

    Preparando o ambiente utilizando MySQL, adicionando dependencias e criando a entidade

---
+++ src/main/java/med/voll/api/medico/Endereco.java
@@ -0,0 +1,19 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+}
